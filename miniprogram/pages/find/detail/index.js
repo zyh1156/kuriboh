a4f5c21a893497b0ea3d445612aa92eda37e20e0
Cytus <1156566771@qq.com>
2021-07-09 17:43:28 +0800

    upload

---
+++ miniprogram/pages/find/detail/index.js
@@ -0,0 +1,82 @@
+// miniprogram/pages/find/detail/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    detail: {
+      title: "",
+      imglist:[]
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.getData(options.id);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  getData(id) {
+    const db = wx.cloud.database();
+    db.collection('news').where({
+      _id: id
+    }).get().then(res => {
+      // res.data 包含该记录的数据
+      let data = res.data[0];
+      console.log(data);
+      this.setData({
+        detail: data
+      })
+    })
+  }
+})
